68c029211df06df74b09643399ba8421fcaf5bb5
mirko-san <41379069+mirko-san@users.noreply.github.com>
2024-07-27 20:34:29 +0900

    :sparkles: client

---
+++ cmd/client/main.go
@@ -0,0 +1,89 @@
+// https://github.com/open-telemetry/opentelemetry-go-contrib/blob/main/instrumentation/net/http/otelhttp/example/client/client.go
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func initTracer(ctx context.Context) (*sdktrace.TracerProvider, error) {
+	exporter, err := newExporter(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// For the demonstration, use sdktrace.AlwaysSample sampler to sample all traces.
+	// In a production application, use sdktrace.ProbabilitySampler with a desired probability.
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exporter),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp, err
+}
+
+func newExporter(ctx context.Context) (sdktrace.SpanExporter, error) {
+	return otlptracehttp.New(ctx)
+}
+
+func main() {
+	ctx := context.Background()
+
+	tp, err := initTracer(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}()
+
+	url := flag.String("server", "http://localhost:7777/hello", "server url")
+	flag.Parse()
+
+	client := http.Client{Transport: otelhttp.NewTransport(http.DefaultTransport)}
+
+	var body []byte
+
+	tr := otel.Tracer("example-otel/cmd/client")
+	err = func(ctx context.Context) error {
+		ctx, span := tr.Start(ctx, "say hello", trace.WithAttributes(semconv.PeerService("ExampleService")))
+		defer span.End()
+		req, _ := http.NewRequestWithContext(ctx, "GET", *url, nil)
+
+		fmt.Printf("Sending request...\n")
+		res, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		body, err = io.ReadAll(res.Body)
+		_ = res.Body.Close()
+
+		return err
+	}(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Response Received: %s\n\n\n", body)
+	fmt.Printf("Waiting for few seconds to export spans ...\n\n")
+	time.Sleep(10 * time.Second)
+	fmt.Printf("Inspect traces on otlptracehttp endpoint\n")
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	go.opentelemetry.io/otel v1.28.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.28.0
 	go.opentelemetry.io/otel/sdk v1.28.0
+	go.opentelemetry.io/otel/trace v1.28.0
 )
 
 require (
@@ -18,7 +19,6 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.28.0 // indirect
 	go.opentelemetry.io/otel/metric v1.28.0 // indirect
-	go.opentelemetry.io/otel/trace v1.28.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
